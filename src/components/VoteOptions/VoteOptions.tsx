d9da67d9ec7e1c0b10b02508eff70b2b8ea08b0a
MaxPress <maxpress96@ukr.net>
2025-05-16 16:22:31 +0300

    add all components

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,34 @@
+import css from "./VoteOptions.module.css";
+import { VoteType } from "../../types/votes";
+
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions(props: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => props.onVote("good")}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => props.onVote("neutral")}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => props.onVote("bad")}>
+        Bad
+      </button>
+      {props.canReset && (
+        <button
+          className={`${css.button} ${css.reset}`}
+          onClick={() => {
+            props.onReset();
+          }}
+        >
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
